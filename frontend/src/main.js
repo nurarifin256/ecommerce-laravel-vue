81f55a5f9caac298710f161acdd41128fcc85693
nurarifin256 <arifinnur402@gmail.com>
2023-07-13 20:22:03 +0700

    install adminlte

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
 /** @format */
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
+import { createPinia } from "pinia";
+
+import "admin-lte/plugins/fontawesome-free/css/all.min.css";
+import "admin-lte/dist/css/adminlte.min.css";
+
+import "admin-lte/plugins/jquery/jquery.min.js";
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js";
+import "admin-lte/dist/js/adminlte.min.js";
 
 const app = createApp(App);
 

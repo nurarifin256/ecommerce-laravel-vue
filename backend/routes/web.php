60bd0460e2d370171443905972b9bf7a21196af7
nurarifin256 <arifinnur402@gmail.com>
2023-09-17 19:58:45 +0700

    implementasi laravel email with queue

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProductImportController;
+use App\Jobs\SendEmailJob;
+use App\Mail\SendEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +23,18 @@ Route::get('/', function () {
 
 Route::get('/products', [ProductImportController::class, 'index'])->name('products.import.index');
 Route::post('/store', [ProductImportController::class, 'store'])->name('products.import.store');
+
+Route::get('send-email', function () {
+    $email = new SendEmail();
+    Mail::to('nur.arifin@adis.co.id')->send($email);
+
+    return 'success';
+});
+
+Route::get('send-email-v2', function () {
+    $data['email'] = 'arifinnur402@gmail.com';
+
+    dispatch(new SendEmailJob($data));
+
+    return 'success';
+});
